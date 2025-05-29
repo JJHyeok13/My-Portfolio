835849eb204b27fafbf3641b9727353eb31ce390
JJHyeok13 <jjinh99@naver.com>
2025-05-29 20:17:36 +0900

    fix: createPage를 통해 생성한 동적페이지 설정 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,6 +16,7 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    "gatsby-plugin-gatsby-cloud",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
