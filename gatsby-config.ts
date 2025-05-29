cbe5587e0ecbd9457c0d97f3b54446e3dced3942
JJHyeok13 <jjinh99@naver.com>
2025-05-29 19:42:43 +0900

    fix: pathPrefix 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,10 @@ import type { GatsbyConfig } from "gatsby";
 import path from "path";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/My-Portfolio",
   siteMetadata: {
-    title: `My Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `정진혁 | 포트폴리오`,
+    siteUrl: `https://JJHyeok13.github.io/My-Portfolio`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
